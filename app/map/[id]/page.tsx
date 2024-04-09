4fd71ec2e73691574b8ab17bb9594c50e99e2719
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-04-09 09:54:40 +0200

    ...

--- app/map/[id]/page.tsx
+++ app/map/[id]/page.tsx
@@ -1,22 +1,6 @@
-'use client'
-
-import { useParams, useSearchParams } from "next/navigation"
-
 const page = () => {
-
-    const searchParams = useSearchParams();
-    const params = useParams();
-    const id = params.id;
-    const filters = searchParams.getAll('filter');
-    const view = searchParams.get('view') ? searchParams.get('view') : 'marker';
-
     return (
-        <>
-            <div>{"Map " + id}</div>
-            <h1 className="font-bold">Filters:</h1>
-            <div>{filters.map(filter => <h2 key={filter[0]}>{filter}</h2>)}</div>
-            <h2>{view}</h2>
-        </>
+        <h1>Specific map</h1>
     )
 }
 export default page
