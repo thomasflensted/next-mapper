6f80816c42a54b3714d4f895d771d2c52f0770e7
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-04-18 15:48:53 +0200

    removed env file

--- auth.ts
+++ auth.ts
@@ -1,6 +1,22 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import vercelPostgresAdapter from "./app/lib/postgresAdapter";
+
 export const { handlers, auth } = NextAuth({
-    providers: [GitHub],
-    secret: process.env.AUTH_SECRET
+    debug: true,
+    secret: process.env.AUTH_SECRET,
+    adapter: vercelPostgresAdapter(),
+    providers: [
+        GitHub({
+            clientId: process.env.AUTH_GITHUB_ID as string,
+            clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
+                },
+            },
+        }),
+    ],
 })
