86a2c90da08e6cf9a601de1d14e117d5831a3efb
Thomas Flensted <thomasflenstedjensen@gmail.com>
2024-04-25 11:40:49 +0200

    fixed double quotes

--- app/ui/frontpage/TextBlockOne.tsx
+++ app/ui/frontpage/TextBlockOne.tsx
@@ -4,7 +4,7 @@ const TextBlockOne = () => {
     return (
         <div className="w-3/4 md:w-1/2">
             <p className="text-xs font-light leading-6 text-center text-blue-600 md:text-sm md:leading-8">
-                That <span className="font-medium">amazing local restaurant</span> you visited 8 years ago in Larache, Morocco? That <span className="font-medium">beautiful view </span>you've been thinking about since your trip to Kyrgyzstan? That <span className="font-medium">cute hotel</span> in Vientiane, Laos that you're still fantasizing about? Never forget any of those with
+                That <span className="font-medium">amazing local restaurant</span> you visited 8 years ago in Larache, Morocco? That <span className="font-medium">beautiful view </span>you&apos;ve been thinking about since your trip to Kyrgyzstan? That <span className="font-medium">cute hotel</span> in Vientiane, Laos that you&apos;re still fantasizing about? Never forget any of those with
                 <Link href='/signin' className="font-medium">
                     &nbsp;Mapper.
                 </Link>
